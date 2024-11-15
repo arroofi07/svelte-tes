b0a1d3f599ec7ab80c727327922f50adcaa3a6c8
arovi <arrooficr7@gmail.com>
2024-11-16 00:21:17 +0700

    second change2

--- src/routes/crud/+page.svelte
+++ src/routes/crud/+page.svelte
@@ -1,103 +1,106 @@
-<script lang="ts" >
-import '$src/app.css'
+<script lang="ts">
+	import '$src/app.css';
 
-type Person =  {
-    name: string,
-    umur: string
-    email: string
-  }
+	type Person = {
+		name: string;
+		umur: string;
+		email: string;
+	};
 
-  let peoples = $state([
-  {name: 'arovi', umur: '20', email:'arofi@gmail.com'},
-  {name: 'axe', umur: '16', email:'axe@gmail.com'}
-  ])
+	let peoples = $state([
+		{ name: 'arovi', umur: '20', email: 'arofi@gmail.com' },
+		{ name: 'axe', umur: '16', email: 'axe@gmail.com' }
+	]);
 
-    let selected: any = $state<Person>()
-    let newPerson = $state({name: '', umur: '',  email: ''})
-        
-   function handleSelect() {
-    newPerson ={
-        name: selected?.name ?? '',
-        umur: selected?.umur ?? '',
-        email: selected?.email ?? '',
-      }
-   }
-    
+	let selected: any = $state<Person>();
+	let newPerson = $state({ name: '', umur: '', email: '' });
 
-    // logic
-    // create
-    const createPeople = ()=> {
-      peoples.push(newPerson)
-      clearInput()
-    }
-    // update
-    const updatePeople = ()=> {
-        const index = peoples.indexOf(selected!)
-        peoples[index] = {name : newPerson.name, umur: newPerson.umur, email: newPerson.email}
-      clearInput()
-      }
+	function handleSelect() {
+		newPerson = {
+			name: selected?.name ?? '',
+			umur: selected?.umur ?? '',
+			email: selected?.email ?? ''
+		};
+	}
 
-    // // delete
-    const deletePeople = ()=> {
-      peoples = peoples.filter(p => p.name !== newPerson.name || p.umur !== newPerson.umur || p.email !== newPerson.email )
-      clearInput()
-    }
+	// logic
+	// create
+	const createPeople = () => {
+		peoples.push(newPerson);
+		clearInput();
+	};
+	// update
+	const updatePeople = () => {
+		const index = peoples.indexOf(selected!);
+		peoples[index] = { name: newPerson.name, umur: newPerson.umur, email: newPerson.email };
+		clearInput();
+	};
 
+	// // delete
+	const deletePeople = () => {
+		peoples = peoples.filter(
+			(p) => p.name !== newPerson.name || p.umur !== newPerson.umur || p.email !== newPerson.email
+		);
+		clearInput();
+	};
 
-
-// membersihkn data di input
-function clearInput (){
-  newPerson = {name: '', umur: '',  email: ''}
-}
+	// membersihkn data di input
+	function clearInput() {
+		newPerson = { name: '', umur: '', email: '' };
+	}
 </script>
 
-
 <!-- pilih data data -->
-<div class="m-10 " >
-  <h1 class="font-bold capitalize">pilih data yg ingin  diubah atau dihapus</h1>
-  <select class="border-2 border-red-500" name="" id="" bind:value={selected} onchange={handleSelect}> 
-    {#each peoples as people }
-    <option value={people}>{people.name}, {people.umur}, {people.email}</option>
-    {/each}
-  </select>
-  <br>
+<div class="m-10">
+	<h1 class="font-bold capitalize">pilih data yg ingin diubah atau dihapus</h1>
+	<select
+		class="border-2 border-red-500"
+		name=""
+		id=""
+		bind:value={selected}
+		onchange={handleSelect}
+	>
+		{#each peoples as people}
+			<option value={people}>{people.name}, {people.umur}, {people.email}</option>
+		{/each}
+	</select>
+	<br />
 </div>
 
-
 <!-- input -->
-<div class="m-10  ">
-  <div class="" >
-    <label for="">Name :</label>
-    <input type="text" class="border-black border-2" bind:value={newPerson.name} >
-  </div>
-  <div class="" >
-    <label for="">Umur :</label>
-    <input type="text" class="border-black border-2" bind:value={newPerson.umur} >
-  </div>
-  <div class="" >
-    <label for="">Email :</label>
-    <input type="text" class="border-black border-2" bind:value={newPerson.email} >
-  </div>
+<div class="m-10">
+	<div class="">
+		<label for="">Name :</label>
+		<input type="text" class="border-2 border-black" bind:value={newPerson.name} />
+	</div>
+	<div class="">
+		<label for="">Umur :</label>
+		<input type="text" class="border-2 border-black" bind:value={newPerson.umur} />
+	</div>
+	<div class="">
+		<label for="">Email :</label>
+		<input type="text" class="border-2 border-black" bind:value={newPerson.email} />
+	</div>
 </div>
 
 <!-- button -->
-<div class="m-10" >
-  <button class="bg-green-500 p-2" onclick={createPeople} >Create</button>
-  <button class="bg-yellow-500 p-2" onclick={updatePeople} >Update</button>
-  <button class="bg-red-500 p-2" onclick={deletePeople} >Delete</button>
+<div class="m-10">
+	<button class="bg-green-500 p-2" onclick={createPeople}>Create</button>
+	<button class="bg-yellow-500 p-2" onclick={updatePeople}>Update</button>
+	<button class="bg-red-500 p-2" onclick={deletePeople}>Delete</button>
 </div>
 
-<br>
+<br />
 
 <!-- list data -->
-<div class="m-10" >
-  <h1>List Data</h1>
-  {#each peoples as people }
-      <p>Name: {people.name}</p>
-      <p>Name: {people.umur}</p>
-      <p>Name: {people.email}</p>
-      <br>
-      {/each}
-    </div>
-<br>
+<div class="m-10">
+	<h1>List Data</h1>
+	{#each peoples as people}
+		<p>Name: {people.name}</p>
+		<p>Name: {people.umur}</p>
+		<p>Name: {people.email}</p>
+		<br />
+	{/each}
+</div>
+<br />
 <a href="/todoList">Buat Jadwalmu</a>
--- src/routes/todoList/+page.svelte
+++ src/routes/todoList/+page.svelte
@@ -1,58 +1,62 @@
-  <script lang="ts" > 
-import '$src/app.css'
-import todoStore from './todosStore';
+<script lang="ts">
+	import '$src/app.css';
+	import todoStore from './todosStore';
 
-  let todos: any = [];
-  todoStore.subscribe((value) => {
-    todos = value;
-  });
+	let todos: any = [];
+	todoStore.subscribe((value) => {
+		todos = value;
+	});
 
-  function addTodo() {
-    todoStore.addItem();
-  }
+	function addTodo() {
+		todoStore.addItem();
+	}
 
-  function removeTodo(index: number) {
-    todoStore.removeItem(index);
-  }
+	function removeTodo(index: number) {
+		todoStore.removeItem(index);
+	}
 </script>
 
+<div class="flex w-full items-center justify-center">
+	<div
+		id="box-todo"
+		class="mt-16 flex w-[50%] flex-col items-center justify-center rounded-[15px] border-[1px] border-black bg-slate-100 p-10"
+	>
+		<h1 class="mt-10 text-center text-[20px] font-bold">Buat Jadwalmu Hari ini</h1>
+		{#each $todoStore as todo, index}
+			<h1 class="text-[25px] font-bold">{index + 1}</h1>
+			<p>
+				<input
+					style="text-shadow: 0 0 10px violet;"
+					bind:value={todos[index]}
+					class="mb-5 ml-10 rounded-[4px] border-2 border-black p-2 font-semibold"
+				/>
+				<button
+					style="box-shadow: 0 0 10px black;"
+					class="-mr-10 ml-5 w-16 rounded-[5px] border-[1px] border-rose-500 bg-white p-2 hover:bg-black"
+					on:click={() => removeTodo(index)}>❌</button
+				>
+			</p>
+		{/each}
 
-<div class="flex justify-center items-center w-full ">
-    <div id="box-todo" class="flex justify-center items-center mt-16 p-10  rounded-[15px] flex-col bg-slate-100 border-[1px] border-black w-[50%]  "  >
-      <h1 class="text-center font-bold text-[20px] mt-10 "  >Buat Jadwalmu Hari ini </h1>
-      {#each $todoStore as todo, index}
-      <h1 class="text-[25px] font-bold  " >{index + 1}</h1>
-      <p>
-        <input
-        style="text-shadow: 0 0 10px violet;" 
-        bind:value={todos[index]}
-        class="border-2 border-black font-semibold p-2 ml-10 rounded-[4px] mb-5" />
-        <button 
-        style="box-shadow: 0 0 10px black;"
-        class="bg-white border-[1px] border-rose-500 -mr-10 ml-5 hover:bg-black w-16 rounded-[5px] p-2 "
-        on:click={() => removeTodo(index)}>❌</button>
-  </p>
-{/each}
-
-<button
-style="box-shadow: 0 0 10px black;" 
-class="bg-white  border-[1px] animate-bounce hover:bg-black mt-3 border-violet-600 w-16 rounded-[5px] p-2  " 
-on:click={addTodo}>➕</button>
+		<button
+			style="box-shadow: 0 0 10px black;"
+			class="mt-3 w-16 animate-bounce rounded-[5px] border-[1px] border-violet-600 bg-white p-2 hover:bg-black"
+			on:click={addTodo}>➕</button
+		>
+	</div>
 </div>
-</div>
-
 
 <div class="m-10 text-center">
-  <a href="/" 
-    class="inline-block bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition font-semibold">
-    Kembali Ke CRUD
-  </a>
+	<a
+		href="/"
+		class="inline-block rounded bg-blue-500 px-6 py-3 font-semibold text-white transition hover:bg-blue-600"
+	>
+		Kembali Ke CRUD
+	</a>
 </div>
 
-
-
 <style>
-  #box-todo{
-    box-shadow: 0 0 10px black;
-}
+	#box-todo {
+		box-shadow: 0 0 10px black;
+	}
 </style>
